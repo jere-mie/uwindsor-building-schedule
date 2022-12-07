9721bd64e2306f04c924a948f7997f2b56d68377
Jeremie <jeremiedevelops@gmail.com>
2022-12-06 21:24:50 -0500

    parser done, generates JS

--- parse.py
+++ parse.py
@@ -1,28 +1,39 @@
-import pprint
 import re
-pp = pprint.PrettyPrinter(indent=4)
+
+
 with open('F22.txt', 'r') as f:
+    # each room booking starts with "Section"
     text = f.read().split('Section')
+
+    # removing trailing newlines
     text = [i.replace('\n', '').strip() for i in text]
+
+    # removing online courses
     text = filter(lambda x: 'synchronous' not in x.lower(), text)
+    
+    # removing trailing stuff
     text = [re.sub(r',\w+', '', i) for i in text]
+
+    # removing more trailing stuff
     text = [re.sub(r' \(-\).+', '', i) for i in text]
+
+    # removing dates
     text = [re.sub(r'\d\d\d\d-\d\d-\d\d', '', i) for i in text]
-    text = [re.search(r'(M|T|W|TH|F|MW|TTH) (\d\d:\d\d) (AM|PM) (\d\d:\d\d(AM|PM))(BiologyBuilding|Erie Hall|Dillon Hall|Toldo HealthEducationCtr|Chrysler HallSouth|Chrysler HallNorth|OdetteBuilding|LambtonTower|Essex Hall|MemorialHall|HK Building|EducationBuilding|LeddyLibrary|West Library|FreedomWay|JackmanDramatic ArtCntre)[ ]*(B\d+|G\d+|\d+)', i) for i in text]
+
+    # grabbing the info
+    text = [re.search(r"(M|T|W|TH|F|MW|TTH) (\d\d:\d\d) (AM|PM) (\d\d:\d\d(AM|PM))(BiologyBuilding|Erie Hall|Dillon Hall|Toldo HealthEducationCtr|Chrysler HallSouth|Chrysler HallNorth|OdetteBuilding|LambtonTower|Essex Hall|MemorialHall|HK Building|EducationBuilding|LeddyLibrary|West Library|FreedomWay|JackmanDramatic ArtCntre|O'NeilMedicalEduc Centr)[ ]*(B\d+|G\d+|\d+)", i) for i in text]
+
+    # filtering out stuff
     text = [i.groups() if i is not None else '' for i in text]
     text = list(filter(lambda x: x != '', text))
-    # groups = [i[5] for i in text]
 
+# out file
+f = open('F22.js', 'w')
 
-# pp.pprint(text)
-# print(text)
-for i in text:
-    # print(f'|{i}|')
-    print(f'{{"day":"{i[0]}", "start":"{i[1]+i[2]}", "end":"{i[3]}", "building":"{i[5]}", "room":"{i[6]}"}},')
-    # print('------')
-print(len(text))
-# for i in groups:
-#     print(f'|{i}|\n---')
-# ggroups = set(groups)
-# for i in ggroups:
-#     print(f'|{i}|\n----------')
+# writing the JS object
+f.write("const F22data = [\n")
+for i in range(len(text)):
+    f.write(f'    {{"day":"{text[i][0]}", "start":"{text[i][1]+text[i][2]}", "end":"{text[i][3]}", "building":"{text[i][5]}", "room":"{text[i][6]}"}}' + ',\n' if i != len(text)-1 else '\n')
+f.write(']')
+
+print(f'Successfully parsed {len(text)} room bookings!')
